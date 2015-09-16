e54fc7128d42e6e2a42e44855a83282cc34499c1
Brendan <brendan.metzger@gmail.com>
2015-09-15 19:00:47 -0500

    got a perspective working

--- perspective.js
+++ perspective.js
@@ -1,34 +1,14 @@
+var paper;
 bloc.prepare('perspective', function () {
-  var paper = new SVG(document.body, {
+  paper = new SVG(document.body, {
     height: coords.h,
     width: coords.w,
     id: 'trace'
   });
   
-  var vanishing_point = paper.createElement('circle', {
-    cx: coords.cx,
-    cy: Math.floor(coords.h / 4 * 3),
-    r: 1
-  });
   
   var grid = paper.createElement('g', {'class': 'grid'});
-  var note = paper.createElement('g', {'class': 'annotation'});
-  
-  var a = paper.createElement('line', {
-    x1: vanishing_point.get('cx'),
-    y1: vanishing_point.get('cy'),
-    x2: 0,
-    y2: coords.h
-  }, grid);
-
-  var b = paper.createElement('line', {
-    x1: vanishing_point.get('cx'),
-    y1: vanishing_point.get('cy'),
-    x2: coords.w,
-    y2: coords.h
-  }, grid);
-  
-  console.log(intersection(a, b));
+  grid = drawPerspective(grid, Math.floor(coords.h / 4 * 2.5));
 });
 
 
@@ -49,7 +29,60 @@ var intersection = function (lineA, lineB) {
   var numx = ( ( (x1*y2) - (y1*x2) ) * ( x3 - x4 ) ) - ( ( x1 - x2 ) * ( (x3*y4) - (y3*x4) ) );
   var numy = ( ( (x1*y2) - (y1*x2) ) * ( y3 - y4 ) ) - ( ( y1 - y2 ) * ( (x3*y4) - (y3*x4) ) );
   var denom = ((x1 - x2) * (y3 - y4)) - ((y1 - y2) * (x3 - x4));
-
+  if (denom === 0) return 0;
   return [numx / denom, numy / denom];
 };
 
+function drawPerspective(grid, horizon) {
+  
+  var guide = paper.createElement('line', {
+    style: 'stroke:none',
+    x1:0,
+    y1: horizon,
+    x2: coords.w,
+    y2: coords.h
+  });
+
+  var density = 10;
+  var vlines = [];
+  var hlines = [];
+  var space = coords.w / density;
+  
+  
+  for (var i = (-4 * density); i <= (density * 8); i++) {
+    vlines[i] = paper.createElement('line', {
+      x1: coords.cx,
+      y1: horizon,
+      x2: space * i,
+      y2: coords.h
+    }, grid);
+    
+    var intersect = intersection(guide, vlines[i]);
+    if (intersect[1] > horizon) {
+      
+      hlines.push(paper.createElement('line', {
+        x1: 0,
+        y1: intersect[1],
+        x2: coords.w,
+        y2: intersect[1]
+      }, grid));
+    }
+    
+    var trim = intersection(hlines[0], vlines[i]);
+    
+    vlines[i].set({
+      x1: trim[0],
+      y1: trim[1]
+    });
+    
+  }
+  
+  return grid;
+}
+
+function movePerspective(input) {
+  var grid = paper.createElement('g', {'class': 'grid'});
+  paper.element.replaceChild(grid, paper.element.querySelector('g.grid'));
+  
+  grid = drawPerspective(grid, Math.floor((coords.h * (input.value / 100)) / 4 * 2.5));
+}
